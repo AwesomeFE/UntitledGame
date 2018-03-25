f77d81a37385f2519377b87c480dd266844c2567
Roger <worfandsheep@qq.com>
2018-03-25 17:32:14 +0800

    prefect moving sheep

--- client/src/mobile/gameComponents/ImportMesh/handler.js
+++ client/src/mobile/gameComponents/ImportMesh/handler.js
@@ -1,6 +1,6 @@
 import { Vector3 } from "babylonjs";
 
-const minSecond = 50;
+const minMS = 10;
 
 export function onPositionChange(_this, oldValue) {
   if(!_this.prePosition) {
@@ -17,23 +17,24 @@ function stopMoving(_this) {
 }
 
 function startMoving(_this) {
-  const { position, prePosition, speed } = _this;
-  const { x: newX, z: newZ } = position;
-  const { x: oldX, z: oldZ } = prePosition;
-
-  console.log('newX: ' + newX, 'newZ: ' + newZ);
-
-  const offsetX = newX - oldX;
-  const offsetZ = newZ - oldZ;
-  const distanceX = Math.abs(offsetX);
-  const distanceZ = Math.abs(offsetZ);
-  const sin = Math.sin(Math.atan(distanceZ / distanceX));
-  const cos = Math.cos(Math.atan(distanceZ / distanceX));
-  const dirX = !isNaN(offsetX * Math.abs(1 / offsetX)) ? offsetX * Math.abs(1 / offsetX) : 1;
-  const dirZ = !isNaN(offsetZ * Math.abs(1 / offsetZ)) ? offsetZ * Math.abs(1 / offsetZ) : 1;
-
   _this.movingTimer = setInterval(() => {
-    const unitSpeed = new Vector3(cos * speed / minSecond * dirX, 0, sin * speed / minSecond * dirZ);
+    const { position, prePosition, speed } = _this;
+    const { x: newX, z: newZ } = position;
+    const { x: oldX, z: oldZ } = prePosition;
+
+    const offsetX = newX - oldX;
+    const offsetZ = newZ - oldZ;
+    const distanceX = Math.abs(offsetX);
+    const distanceZ = Math.abs(offsetZ);
+    const sin = Math.sin(Math.atan(distanceZ / distanceX));
+    const cos = Math.cos(Math.atan(distanceZ / distanceX));
+    const dirX = !isNaN(offsetX * Math.abs(1 / offsetX)) ? offsetX * Math.abs(1 / offsetX) : 1;
+    const dirZ = !isNaN(offsetZ * Math.abs(1 / offsetZ)) ? offsetZ * Math.abs(1 / offsetZ) : 1;
+    
+    const speedX = cos * speed / 1000 * minMS * dirX;
+    const speedZ = sin * speed / 1000 * minMS * dirZ;
+    const speedY = Math.abs(Math.min(speedX, speedZ)) * -1;
+    const unitSpeed = new Vector3(speedX, speedY, speedZ);
 
     _this.container.meshes[0].moveWithCollisions(unitSpeed);
     _this.prePosition = _this.container.meshes[0].position;
@@ -42,10 +43,8 @@ function startMoving(_this) {
       Math.abs(_this.position.x - _this.prePosition.x) < 0.1 &&
       Math.abs(_this.position.z - _this.prePosition.z) < 0.1;
 
-    console.log((_this.position.x - _this.prePosition.x).toFixed(2), (_this.position.z - _this.prePosition.z).toFixed(2));
-
     if(isAttachDestination) {
       stopMoving(_this);
     }
-  }, minSecond);
+  }, minMS);
 }
