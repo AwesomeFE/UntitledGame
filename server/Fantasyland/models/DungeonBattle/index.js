cae3c896339863d499000a9646ee33cf4e2a47f1
Roger <worfandsheep@qq.com>
2018-05-31 14:49:39 +0800

    working

--- server/Fantasyland/models/DungeonBattle/index.js
+++ server/Fantasyland/models/DungeonBattle/index.js
@@ -1,9 +1,9 @@
 import schema from './schema';
 import mongoose, { Model } from 'mongoose';
 
-class DungeonChapterModel extends Model {
+class DungeonBattleModel extends Model {
 }
 
-schema.loadClass(DungeonChapterModel);
+schema.loadClass(DungeonBattleModel);
 
-export const dungeonChapter = mongoose.model('dungeon-chapter', schema);
+export const DungeonBattle = mongoose.model('dungeon-battle', schema);
--- server/Fantasyland/models/index.js
+++ server/Fantasyland/models/index.js
@@ -2,4 +2,5 @@ export { User, Entry } from '../../common/models';
 export { Player } from './Player';
 export { Enemy } from './Enemy';
 export { Dungeon } from './Dungeon';
+export { DungeonBattle } from './DungeonBattle';
 export { DungeonChapter } from './DungeonChapter';
